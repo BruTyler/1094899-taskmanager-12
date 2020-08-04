7c312ef90161270552ff446951083b2e8b78e686
Anton Churkin <afrg@yandex.ru>
2020-08-04 18:07:39 +0500

    single task is got dynamic content

--- src/mocks/task.js
+++ src/mocks/task.js
@@ -4,19 +4,27 @@ import {pickRandomElement, pickRandomBool, pickRandomDate} from '../utils.js';
 const descriptions = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`];
 const colors = Object.values(Color);
 
+const generateRepeatingDays = (dueDate) => {
+  return {
+    mo: false,
+    tu: false,
+    we: false,
+    th: false,
+    fr: false,
+    sa: false,
+    su: dueDate !== null
+      ? pickRandomBool()
+      : false
+  };
+};
+
 export function generateRandomTask() {
+  const dueDate = pickRandomDate();
+
   const task = {
     description: pickRandomElement(descriptions),
-    dueDate: pickRandomDate(),
-    repeatingDays: {
-      mo: pickRandomBool(),
-      tu: pickRandomBool(),
-      we: pickRandomBool(),
-      th: pickRandomBool(),
-      fr: pickRandomBool(),
-      sa: pickRandomBool(),
-      su: pickRandomBool()
-    },
+    dueDate,
+    repeatingDays: generateRepeatingDays(dueDate),
     color: pickRandomElement(colors),
     isFavorite: pickRandomBool(),
     isArchive: pickRandomBool(),
--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,59 @@
-function generateRandomInt(min, max) {
+const generateRandomInt = (min, max) => {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
-}
+};
 
-export function pickRandomElement(array) {
+export const pickRandomElement = (array) => {
   if (array === undefined || array === null || array.length === 0) {
     throw new Error(`Incorrect array`);
   }
   const randomIndex = generateRandomInt(0, array.length - 1);
   return array[randomIndex];
-}
+};
 
-export function pickRandomBool() {
-  return generateRandomInt(0, 1) === 1;
-}
+export const pickRandomBool = () => {
+  return Boolean(generateRandomInt(0, 1));
+};
 
-export function pickRandomDate() {
+export const pickRandomDate = () => {
   if (pickRandomBool() === true) {
     return null;
   }
 
+  const dayOffset = 7;
+  const randomDayOffset = generateRandomInt(-dayOffset, +dayOffset);
+
   const currentDate = new Date();
-  const weakOffset = 1000 * 60 * 60 * 24 * 7;
-  const minTime = currentDate.getTime() - weakOffset;
-  const maxTime = currentDate.getTime() + weakOffset;
-  const randomTime = generateRandomInt(minTime, maxTime);
-  return new Date(randomTime);
-}
+  currentDate.setDate(currentDate.getDate() + randomDayOffset);
+
+  return new Date(currentDate);
+};
+
+export const isTaskExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = new Date();
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
+export const isTaskExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = new Date();
+
+  return currentDate.toDateString() === dueDate.toDateString();
+};
+
+export const isTaskRepeating = (repeating) => {
+  return Object.values(repeating).includes(true);
+};
+
+export const humanizeTaskDueDate = (dueDate) => {
+  return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
+};
 
