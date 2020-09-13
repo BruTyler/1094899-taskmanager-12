0428933ed352cf6136d57996cf5c9978731c5c30
Anton Churkin <afrg@yandex.ru>
2020-09-14 00:46:17 +0500

    add ServiceWorker

--- src/main.ts
+++ src/main.ts
@@ -66,3 +66,12 @@ api.getTasks()
     render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
     siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
   });
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      console.log(`ServiceWorker available`); // eslint-disable-line
+    }).catch(() => {
+      console.error(`ServiceWorker isn't available`); // eslint-disable-line
+    });
+});
